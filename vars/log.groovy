1c6cad1b912ea62db3e0e5178d2ec70e157f2fb9
maheswarsarma <31466024+maheswarsarma@users.noreply.github.com>
2023-07-24 00:21:38 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,9 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+def call() {
+    def info(message) {
+        echo "INFO: ${message}"
+        }
 
-def warning(message) {
-    echo "WARNING: ${message}"
+    def warning(message) {
+        echo "WARNING: ${message}"
+    }
 }
